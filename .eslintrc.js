7330de6e2c19b3826ab249e9d2ef2cd189e0edb3
hezaichang <heyin223@gmail.com>
2019-09-03 21:58:02 +0800

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,48 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
     root: true,
+    parserOptions: {
+        parser: "babel-eslint"
+    },
     env: {
+        browser: true,
         node: true
     },
-    // 'extends': [
-    //     'plugin:vue/essential',
-    //     'eslint:recommended',
-    // ],
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    extends: ["plugin:vue/essential", "airbnb-base"],
+    // required to lint *.vue files
     plugins: [
         "vue"
     ],
-    "overrides": [
-        {
-            "files": ["*.vue"],
-            "rules": {
-                "indent": "off"
+    // check if imports actually resolve
+    settings: {
+        "import/resolver": {
+            webpack: {
+                config: "build/webpack.base.conf.js"
             }
         }
-    ],
-    extends: ["plugin:vue/essential", "airbnb-base", 'eslint:recommended'],
+    },
+    // add your custom rules here
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        // don"t require .vue extension when importing
+        "import/extensions": ["error", "always", {
+            js: "never",
+            vue: "never",
+            jsx: "never"
+        }],
+        // disallow reassignment of function parameters
+        // disallow parameter object manipulation except for specific exclusions
+        "no-param-reassign": ["off"],
+        "consistent-return": ["off"],
+        // allow optionalDependencies
+        "import/no-extraneous-dependencies": ["error", {
+            optionalDependencies: ["test/unit/index.js"]
+        }],
+        // allow debugger during development
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        // custom
         "indent": ["error", 4],
         "vue/script-indent": [
             "error",
@@ -38,6 +59,8 @@ module.exports = {
         "semi": ["error", "never"],
         "semi-style": ["error", "first"],
         "quotes": ["error", "single"],
+        "no-tabs": 0,
+        "no-new": 0,
         "comma-dangle": ["error", "never"],
         "space-before-function-paren": ["error", "always"],
         "one-var": ["error", "never"],
@@ -55,11 +78,27 @@ module.exports = {
         }],
         "max-len": ["error", {
             code: 120
-        }]
+        }],
+        "no-underscore-dangle": ["off"],
+        "no-console": ["off"],
+        "no-mixed-operators": ["off"],
+        "global-require": ["off"],
+        "func-names": ["off"]
     },
-    parserOptions: {
-        parser: 'babel-eslint'
-    },
+    "overrides": [
+        {
+            "files": ["*.vue"],
+            "rules": {
+                "indent": "off"
+            }
+        },
+        {
+            "files": ["*.js"],
+            "rules": {
+                "vue/script-indent": "off"
+            }
+        }
+    ],
     globals: {
         MPing: 'readonly'
     }
